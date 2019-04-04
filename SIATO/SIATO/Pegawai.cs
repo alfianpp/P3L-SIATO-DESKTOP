f0fb2e578a073aed1c2d576fc640e74ac0db2c78
Alfian Putra Persada <alfianputrapersada@gmail.com>
2019-04-04 08:21:20 +0700

    Membuat form login

---
+++ SIATO/SIATO/Pegawai.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SIATO
+{
+    public class Pegawai
+    {
+        public int id { get; set; }
+        public string username { get; set; }
+        public string nama { get; set; }
+        public string nomor_telepon { get; set; }
+        public string alamat { get; set; }
+        public double gaji { get; set; }
+        public int role { get; set; }
+        public string api_key { get; set; }
+    }
+}
